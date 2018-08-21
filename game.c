a56ce35829a9badd834e3545641b139d63e05500
Raz Loup <razlopa@gmail.com~>
2018-08-21 16:31:14 +0300

    fixed sizes and prints

--- game.c
+++ game.c
@@ -4,29 +4,43 @@
 #include<sys/syscall.h>
 #include<linux/random.h>
 
-void getRandom(char *code) {
-	int i;
-	int r;
-	printf("code@:%s", code);
+int getRandom() {
+	unsigned long r;
 	syscall(SYS_getrandom, &r, 4, 1);
 	r = r % 9000 + 1000;
 	if (r < 0) {
 		r *= -1;
 	}
-	for (i = 3; i >= 0; i--) {
-		code[i] = r % 10;
-		r /= 10;
-	}
-	printf("code!:%s", code);
+	printf("randomNumber:%ld\n", r);
+	return r;
 }
+
+void intToChar(int number, char *arr) {
+	int i;
+	for (i = 3; i >= 0; i--) {
+		arr[i] = number % 10 + '0';
+		number /= 10;
+	}
+}
+
+int charToInt(char *arr) {
+	int i;
+	int number = 0;
+	for (i = 0; i < 4; i++) {
+		number += arr[i];
+		number *= 10;
+	}
+	return number;
+}
+
+//int checkGuess(char *result, int code, int guess) {
+//	int win = 0;
+//}
 
 int checkGuess(char *result, char *code, char *guess) {
 	int win = 0;
 	int j;
-	printf("\n@@@@@@@\n");
 	for (j = 0; j < 4; j++) {
-		printf("code:%d, ", code[j]);
-		printf("guess:%d\n", guess[j]);
 		if (code[j] == guess[j]) {
 			result[j] = 'X';
 		}
@@ -35,7 +49,6 @@ int checkGuess(char *result, char *code, char *guess) {
 			win = 1;
 		}
 	}
-	printf("@#@#@#\n");
 	return win;
 }
 
@@ -55,27 +68,21 @@ int main() {
 	char loseMsg[] = "\nYou lose!\n";
 	char result[] = "OOOO\n";
 	char guess[5];
+	char code[5];
 	int i;
-	char code[] = "0000\n";
-	getRandom(code);
+	int number;
+	number = getRandom();
+	intToChar(number, code);
 	write(1, welcomeMsg, sizeof(welcomeMsg) -1);
 	write(1, instructionsMsg, sizeof(instructionsMsg) -1);
 	write(1, insertGuessMsg, sizeof(insertGuessMsg) -1);
-	read(0, guess, 4);
-	printf("code:%s\n", code);
-	printf("guess:%s\n", guess);
+	read(0, &guess, 5);
 	win = checkGuess(result, code, guess);
-	printf("****\n");
 	write(1, result, sizeof(result) -1);
-	for (i = 1; i < 10 && win == 1; i++) {
-		//initResult(result);
-		printf("####\n");
+	for (i = 0; i < 9 && win == 1; i++) {
 		write(1, insertGuessMsg, sizeof(insertGuessMsg) -1);
-		printf("$$$$\n");
-		read(0, guess, 4);
-		printf("^^^^\n");
-		win = checkGuess(result, code, guess);
-		printf("&&&&\n");
+		read(0, &guess, 5);
+		win = checkGuess(result, code, guess);;
 		write(1, result, sizeof(result) -1);
 		i++;
 	}
